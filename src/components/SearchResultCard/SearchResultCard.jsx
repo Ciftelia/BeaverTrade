94f65e092261083ce4a09d491332b9d678497d4f
Ciftelia <klodiana.kamberi87@gmail.com>
2024-12-27 11:36:29 -0500

    added export statement

---
+++ src/components/SearchResultCard/SearchResultCard.jsx
@@ -0,0 +1,10 @@
+// To build this component, we need to know the structure of the data that we will be receiving from the API.
+// For now, go by the Figma sample for Threadco.
+// Try to build a copy of it, it doesn't need to be perfect, just has to contain most or all of the data.
+import "./SearchResultCard.css";
+
+const SearchResultCard = () => {
+  return <></>;
+};
+
+export default SearchResultCard;
