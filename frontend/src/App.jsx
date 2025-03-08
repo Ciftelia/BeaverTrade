eca2d041b9e4baa31047d05e4085b0010ab3fcfa
Ciftelia <klodiana.kamberi87@gmail.com>
2025-03-08 13:46:48 -0500

    App now works with AppProvider, now to add DashboardLayout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,42 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { ReactRouterAppProvider } from "@toolpad/core/react-router";
+import { AppProvider } from "@toolpad/core/AppProvider";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import SearchResults from "./components/Search/SearchResults";
 import AboutPage from "./pages/AboutPage";
 import HomePage from "./pages/HomePage";
-import Header from "./components/Header/Header";
+import Layout from "./components/Layout/Layout";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />, // Acts as the parent layout
+    children: [
+      { path: "/", element: <HomePage /> },
+      { path: "/search", element: <SearchResults /> },
+      { path: "/about", element: <AboutPage /> },
+      { path: "*", element: <h1>Not Found</h1> },
+    ],
+  },
+]);
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#E6AF2E",
+    },
+    secondary: {
+      main: "#632B30",
+    },
+  },
+});
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Header />
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <>
-                <HomePage />
-              </>
-            }
-          />
-          <Route path="/search" element={<SearchResults />} />
-          <Route path="*" element={<h1>Not Found</h1>} />
-          <Route path="/about" element={<AboutPage />} />
-        </Routes>
-      </div>
-    </Router>
+    <AppProvider router={router} theme={theme}>
+      <RouterProvider router={router} theme={theme} />
+    </AppProvider>
   );
 }
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,25 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: "#E6AF2E",
-    },
-    secondary: {
-      main: "#632B30",
-    },
-  },
-});
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ThemeProvider theme={theme}>
     <App />
-    </ThemeProvider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
