934cd2620988a2c40140fdce56d0100588eee3a1
Ciftelia <klodiana.kamberi87@gmail.com>
2025-03-08 17:22:29 -0500

    Now Cards, show all data

--- frontend/src/components/SearchResultCard/SearchResultCard.jsx
+++ frontend/src/components/SearchResultCard/SearchResultCard.jsx
@@ -1,37 +1,55 @@
 import "./SearchResultCard.css";
-import { Card, CardContent, Typography } from "@mui/material";
+import { Card, CardContent, CardActionArea, Typography } from "@mui/material";
+import { CardMedia } from "@mui/material";
 
 const SearchResultCard = ({ id, result }) => {
   return (
-    // <>
-    //   <br></br>
-    //   <div>
-    //     <div className="main_info">
-    //       <img src={`http://localhost:3000/images/${result.logo_url}`}></img>
-    //       <div>{result.name}</div>
-    //       <div>{result.description}</div>
-    //     </div>
-    //     <div className="Contact">
-    //       <div>{result.phone_number}</div>
-    //       <div>{result.email}</div>
-    //     </div>
-    //     <div className="location">
-    //       <div>{result.city}</div>
-    //       <div>{result.province}</div>
-    //       <div>{result.address}</div>
-    //       <div>{result.postal_code}</div>
-    //     </div>
-    //   </div>
-    //   <br></br>
-    // </>
-    <>
-      <Card>
-        <CardContent>
-          <Typography variant="h6">{result.name}</Typography>
-          <Typography variant="body2">{result.description}</Typography>
-        </CardContent>
+    <CardActionArea>
+      <Card
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          marginBottom: "20px",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <CardMedia
+          component="img"
+          style={{ width: "150px", height: "150px" }}
+          image={`http://localhost:3000/images/${result.logo_url}`}
+          alt={result.name}
+        />
+        <div style={{ flex: "1", textAlign: "center" }}>
+          <CardContent>
+            <Typography variant="h6">{result.name}</Typography>
+            <Typography variant="body2">{result.description}</Typography>
+          </CardContent>
+          <CardContent
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-between",
+            }}
+          >
+            <Typography variant="body2">{result.phone_number}</Typography>
+            <Typography variant="body2">{result.email}</Typography>
+          </CardContent>
+          <CardContent
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-between",
+            }}
+          >
+            <Typography variant="body2">{result.city}</Typography>
+            <Typography variant="body2">{result.province}</Typography>
+            <Typography variant="body2">{result.address}</Typography>
+            <Typography variant="body2">{result.postal_code}</Typography>
+          </CardContent>
+        </div>
       </Card>
-    </>
+    </CardActionArea>
   );
 };
 
